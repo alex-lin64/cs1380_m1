b6f63405a0c508a9ea611b08ad78d0728a5317db
alex-lin64 <alex_lin@brown.edu>
2024-02-10 01:00:44 +0000

    working on ser and deser arrays and objs

--- distribution/util/serialization.js
+++ distribution/util/serialization.js
@@ -1,13 +1,99 @@
-function serialize(object) {
-  return '';
+// serializes the basics (string, number, boolean)
+const serializeBasics = (basicType) => {
+  const obj = {
+    type: typeof basicType,
+    value: JSON.stringify(basicType),
+  };
+  return JSON.stringify(obj);
+};
+
+// deserializes the basics (string, number, boolean)
+const deserializeBasics = (basicObj) => {
+  const t = basicObj.type;
+  if (t != "string" && t != "number" && t != "boolean") {
+    console.error("Not a basic type");
+    return;
+  }
+  const basic = JSON.parse(basicObj.value);
+  return basic;
+};
+
+// serializes error objects
+const serializeError = (err) => {
+  const errObj = {
+    type: "error",
+    value: err.message,
+  };
+  return JSON.stringify(errObj);
+};
+
+// deserializes error objects
+const deserializeError = (errObj) => {
+  if (errObj.type != "error") {
+    console.error("Not an error object");
+    return;
+  }
+  const err = new Error(errObj.value);
+  return err;
+};
+
+// serializes arrs and any nested objs in the array
+const serializeArray = (arr) => {
+  // for arrays, make sure to loop through each val and serialize each element
+  const arrObj = {
+    type: "array",
+    value: "",
+  };
+};
+
+const serializeObj = (obj) => {
+  // console.log(JSON.stringify(obj));
+
+  // for nested obj's, should loop through and serialize each element
+  return JSON.stringify(obj);
+};
+
+function serialize(obj) {
+  switch (typeof obj) {
+    case "string":
+      return serializeBasics(obj);
+    case "number":
+      return serializeBasics(obj);
+    case "boolean":
+      return serializeBasics(obj);
+    case "symbol":
+    case "undefined":
+    case "object":
+      if (obj instanceof Array) {
+        return serializeArray(obj);
+      }
+      if (obj instanceof Error) {
+        return serializeError(obj);
+      }
+      return serializeObj(obj);
+    case "function":
+  }
+  return "No cases caught";
 }
 
 function deserialize(string) {
-  return {};
+  // console.log(string);
+
+  // convert the jsonify'd format to obj
+  const draft = JSON.parse(string);
+  const t = draft.type;
+
+  if (t == "number" || t == "string" || t == "boolean") {
+    return deserializeBasics(draft);
+  }
+  if (t == "error") {
+    return deserializeError(draft);
+  }
+  // console.log(draft);
+  return draft;
 }
 
 module.exports = {
   serialize: serialize,
   deserialize: deserialize,
 };
-
--- test/serialization.student.test.js
+++ test/serialization.student.test.js
@@ -1,38 +1,8 @@
-const util = require('../distribution').util;
+const util = require("../distribution").util;
 
-test('(0 pts) sample test', () => {
+test("(0 pts) sample test", () => {
   const number = 42;
   const serialized = util.serialize(number);
   const deserialized = util.deserialize(serialized);
   expect(deserialized).toBe(number);
 });
-
-
-test('(0 pts) sample test', () => {
-  const number = 42;
-  const serialized = util.serialize(number);
-  const deserialized = util.deserialize(serialized);
-  expect(deserialized).toBe(number);
-});
-
-test('(0 pts) sample test', () => {
-  const number = 42;
-  const serialized = util.serialize(number);
-  const deserialized = util.deserialize(serialized);
-  expect(deserialized).toBe(number);
-});
-
-test('(0 pts) sample test', () => {
-  const number = 42;
-  const serialized = util.serialize(number);
-  const deserialized = util.deserialize(serialized);
-  expect(deserialized).toBe(number);
-});
-
-test('(0 pts) sample test', () => {
-  const number = 42;
-  const serialized = util.serialize(number);
-  const deserialized = util.deserialize(serialized);
-  expect(deserialized).toBe(number);
-});
-
